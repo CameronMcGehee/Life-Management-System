7a02c808c01d54c6b1ecd806723a02d7016fd7e6
CameronMcGehee <cameronmcgehee23@gmail.com>
2021-10-09 19:02:34 -0400

    config messages actually show now

--- lib/config/defaultMainConfig.php
+++ lib/config/defaultMainConfig.php
@@ -16,6 +16,8 @@
 
     $ULTISCAPECONFIG['allowAccess'] = true;
     $ULTISCAPECONFIG['denyAccessMessage'] = '<h1>UltiScape is current down for maintenance and will be back up in a few moments. Reload the page in a few minutes to try again.</h1>';
-    $ULTISCAPECONFIG['backupMessage'] = '<h1>Ultiscape is currently undergoing a backup and will be back up in a few moments. Reload the page in a few minutes to try again.</h1>';
+
+    $ULTISCAPECONFIG['databaseErrTitle'] = 'UltiScape - No Database Connection';
+    $ULTISCAPECONFIG['databaseErrMessage'] = '<h1>Ultiscape currently cannot connect to the configured database. Reload the page in a few minutes to try again.</h1><p>If you are the owner of this installation, please check the database credentials in your config and the status of the server hosting the database.</p>';
 
 ?>
--- public/php/startSession.php
+++ public/php/startSession.php
@@ -8,7 +8,7 @@
 
     // Check connection and start session
     if (mysqli_connect_errno()) {
-        echo '<html><head><title>Ultiscape - Backup In Progress</title></head><body style="background-color: white;"><span style="width: 100%; height: 100%; display: flex; align-items: center; justify-content: center; text-align: center;"><h1>Ultiscape is currently undergoing a backup and will be back up in a few moments. Reload the page to try again.</h1></span></body></html>';
+        echo '<html><head><title>'.$ULTISCAPECONFIG['databaseErrTitle'].'</title></head><body style="background-color: white;"><span style="width: 100%; height: 100%; display: flex; align-items: center; justify-content: center; text-align: center;"><h1>'.$ULTISCAPECONFIG['databaseErrMessage'].'</h1></span></body></html>';
         exit();
     } else {
         session_start();
