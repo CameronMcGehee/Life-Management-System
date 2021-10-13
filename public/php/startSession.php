11c4b2f1c416924164f20022f5ca391581f8479a
Cameron McGehee <cameronmcgehee23@gmail.com>
2021-10-12 20:51:06 -0400

    add check for alloAccess in config before loading

--- public/php/startSession.php
+++ public/php/startSession.php
@@ -8,8 +8,11 @@
 
     // Check connection and start session
     if (mysqli_connect_errno()) {
-        echo '<html><head><title>'.$ULTISCAPECONFIG['databaseErrTitle'].'</title></head><body style="background-color: white;"><span style="width: 100%; height: 100%; display: flex; align-items: center; justify-content: center; text-align: center;"><h1>'.$ULTISCAPECONFIG['databaseErrMessage'].'</h1></span></body></html>';
+        echo '<html><head><title>'.$ULTISCAPECONFIG['databaseErrTitle'].'</title></head><body style="background-color: white;"><span style="width: 100%; height: 100%; display: flex; align-items: center; justify-content: center; text-align: center;">'.$ULTISCAPECONFIG['databaseErrMessage'].'</span></body></html>';
         exit();
+    } elseif ($ULTISCAPECONFIG['allowAccess'] == false) {
+            echo '<html><head><title>'.$ULTISCAPECONFIG['denyAccessTitle'].'</title></head><body style="background-color: white;"><span style="width: 100%; height: 100%; display: flex; align-items: center; justify-content: center; text-align: center;">'.$ULTISCAPECONFIG['denyAccessMessage'].'</span></body></html>';
+            exit();
     } else {
         session_start();
     }
