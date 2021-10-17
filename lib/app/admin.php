4d3ce9c274898f04ac1336670367b67b649cd855
CameronMcGehee <cameronmcgehee23@gmail.com>
2021-10-17 17:30:35 -0400

    uuid class and admin class almost done

--- lib/app/admin.php
+++ lib/app/admin.php
@@ -1,22 +1,20 @@
 <?php
 
-	// Experimenting with new concept of managing the classes, where you can create an instance of 'admin', set the values of the admin object and then run a set() method that will update the admin in the database rather than the weird way of managing classes before where it is practically functional programming, but encased in a class
-
 	class admin {
 
-		private $setType;
-		private $db;
+		private string $setType;
+		private databaseManager $db;
 
-		public $adminId;
-		public $username;
-		public $password;
-		public $email;
+		public string $adminId;
+		public string $username;
+		public string $password;
+		public string $email;
 		public $surname;
-		public $firstName;
-		public $lastName;
+		public string $firstName;
+		public string $lastName;
 		public $profilePicture;
-		public $allowCZSignIn;
-		public $dateTimeJoined;
+		public bool $allowSignIn;
+		public string $dateTimeJoined;
 		public $dateTimeLeft;
 
 		function __construct(string $adminId = '') {
@@ -30,30 +28,49 @@
 			if ($adminId == '') {
 				// Make a new admin Id from a random id
 				require_once dirname(__FILE__)."/uuid.php";
-				$newUuid = new uuid('admin');
+				$newUuid = new uuid('admin', 'adminId');
 				$newUuid = $newUuid->generatedId;
 			}
 
 			// Fetch from database
 			$fetch = $this->db->select('admin', '*', "WHERE adminId ='$adminId'");
 
-			// If adminId already exists then set the set method type to UPDATE
+			// If adminId already exists then set the set method type to UPDATE and fetch the values for the admin
 			if ($fetch) {
 				$this->setType = 'UPDATE';
 				$this->adminId = $adminId;
 
-				$this->$username = $fetch[0]['username'];
-				$this->$password = $fetch[0]['password'];
-				$this->$email = $fetch[0]['email'];
-				$this->$surname = $fetch[0]['surname'];
-				$this->$firstName = $fetch[0]['firstName'];
-				$this->$lastName = $fetch[0]['lastName'];
-				$this->$profilePicture = $fetch[0]['profilePicture'];
-				$this->$allowCZSignIn = $fetch[0]['allowCZSignIn'];
-				$this->$dateTimeJoined = $fetch[0]['dateTimeJoined'];
-				$this->$dateTimeLeft = $fetch[0]['dateTimeLeft'];
-			}
+				$this->username = $fetch[0]['username'];
+				$this->password = $fetch[0]['password'];
+				$this->email = $fetch[0]['email'];
+				$this->surname = $fetch[0]['surname'];
+				$this->firstName = $fetch[0]['firstName'];
+				$this->lastName = $fetch[0]['lastName'];
+				$this->profilePicture = $fetch[0]['profilePicture'];
+				$this->allowSignIn = $fetch[0]['allowSignIn'];
+				if ($this->allowSignIn == '0') {
+					$this->allowSignIn = false;
+				} else {
+					$this->allowSignIn = true;
+				}
+				$this->dateTimeJoined = $fetch[0]['dateTimeJoined'];
+				$this->dateTimeLeft = $fetch[0]['dateTimeLeft'];
+			// If adminId does not exist then set the set method type to INSERT and inititialize default values
+			} else {
+				$this->setType = 'INSERT';
+				$this->adminId = $newUuid;
 
+				$this->username = '';
+				$this->password = '';
+				$this->email = '';
+				$this->surname = NULL;
+				$this->firstName = '';
+				$this->lastName = '';
+				$this->profilePicture = NULL;
+				$this->allowSignIn = 1;
+				$this->dateTimeJoined = '';
+				$this->dateTimeLeft = NULL;
+			}
 			
 		}
 
--- lib/app/uuid.php
+++ lib/app/uuid.php
@@ -20,20 +20,20 @@
 			$this->db = new databaseManager;
 
 			// Generate the first id
-			$this->generatedId = mt_rand(1, 9999); $n .= uniqid();
+			$this->generatedId = mt_rand(1, 9999).uniqid();
 
 			// Check if there is already an id in the given table in the given column with that id
 			if ($this->db->select($this->table, $this->idColumn, "WHERE $this->idColumn = '$this->generatedId'")) {
-				$this->regenerate;
+				$this->regenerate();
 			}
 		}
 
 		// Regenerate a different id
 		public function regenerate() {
-			$this->generatedId = mt_rand(1, 9999); $n .= uniqid();
+			$this->generatedId = mt_rand(1, 9999).uniqid();
 
 			while ($this->db->select($this->table, $this->idColumn, "WHERE $this->idColumn = '$this->generatedId'")) {
-				$this->generatedId = mt_rand(1, 9999); $n .= uniqid();
+				$this->generatedId = mt_rand(1, 9999).uniqid();
 			}
 		}
 
