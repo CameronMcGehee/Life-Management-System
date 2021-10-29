967bd71f38f1d8b30039005b9739a052c735ba8f
CameronMcGehee <cameronmcgehee23@gmail.com>
2021-10-29 18:00:40 -0400

    update register script to use new config setting names

--- public/admin/scripts/standalone/register.script.php
+++ public/admin/scripts/standalone/register.script.php
@@ -5,34 +5,34 @@
 	// Error checking
 	// Input is set and valid
 	
-	if (!isset($_POST['email']) || strlen($_POST['email']) < (int)$ULTISCAPECONFIG['adminEmailMinLength']) {
+	if (!isset($_POST['email']) || strlen($_POST['email']) < (int)$ULTISCAPECONFIG['emailMinLength']) {
 		header("location: ../../register?e=emailInput");
 		exit();
 	}
 
 	// If enabled, use filter function to make sure email is an actual email
-	if ($ULTISCAPECONFIG['adminUseEmailValidation']) {
+	if ($ULTISCAPECONFIG['useEmailValidation']) {
 		if (filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
 			header("location: ../../register?e=emailInputValidity");
 			exit();
 		}
 	}
 
-	if (!isset($_POST['firstName']) || strlen($_POST['firstName']) < (int)$ULTISCAPECONFIG['adminFirstNameMinLength']) {
+	if (!isset($_POST['firstName']) || strlen($_POST['firstName']) < (int)$ULTISCAPECONFIG['firstNameMinLength']) {
 		header("location: ../../register?e=firstNameInput");
 		exit();
 	}
-	if (!isset($_POST['lastName']) || strlen($_POST['lastName']) < (int)$ULTISCAPECONFIG['adminLastNameMinLength']) {
+	if (!isset($_POST['lastName']) || strlen($_POST['lastName']) < (int)$ULTISCAPECONFIG['lastNameMinLength']) {
 		header("location: ../../register?e=lastNameInput");
 		exit();
 	}
 
-	if (!isset($_POST['username']) || strlen($_POST['username']) < (int)$ULTISCAPECONFIG['adminUsernameMinLength']) {
+	if (!isset($_POST['username']) || strlen($_POST['username']) < (int)$ULTISCAPECONFIG['usernameMinLength']) {
 		header("location: ../../register?e=usernameInput");
 		exit();
 	}
 
-	if (!isset($_POST['password']) || strlen($_POST['password']) < (int)$ULTISCAPECONFIG['adminPasswordMinLength']) {
+	if (!isset($_POST['password']) || strlen($_POST['password']) < (int)$ULTISCAPECONFIG['passwordMinLength']) {
 		header("location: ../../register?e=passwordInput");
 		exit();
 	}
@@ -40,7 +40,7 @@
 	if (!isset($_POST['nameTitle'])) {
 		$nameTitle = '';
 	} elseif ($_POST['nameTitle'] == '') {
-		if (strlen($_POST['nameTitle']) < (int)$ULTISCAPECONFIG['adminnameTitleMinLength']) {
+		if (strlen($_POST['nameTitle']) < (int)$ULTISCAPECONFIG['nameTitleMinLength']) {
 			header("location: ../../register?e=nameTitleInput");
 			exit();
 		}
