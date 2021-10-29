b0719a32f447703e37401dd1a2ede382c145057a
CameronMcGehee <cameronmcgehee23@gmail.com>
2021-10-28 21:31:43 -0400

    fix position of line of code

--- lib/class/adminLoginAttempt.php
+++ lib/class/adminLoginAttempt.php
@@ -111,10 +111,10 @@
 
 				// Insert the values to the database after sanitizing them
 				if ($this->db->insert('adminLoginAttempt', $attributes)) {
-					return true;
-				} elseif ($this->db->getLastError() === '') {
 					// Set the setType to UPDATE since it is now in the database
 					$this->setType = 'UPDATE';
+					return true;
+				} elseif ($this->db->getLastError() === '') {
 					return true;
 				} else {
 					return $this->db->getLastError();
