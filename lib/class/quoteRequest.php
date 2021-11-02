d6c488d4a69391dc66bc4063ae97d60cd7df0a4a
CameronMcGehee <cameronmcgehee23@gmail.com>
2021-11-02 15:01:44 -0400

    quoteRequest class

---
+++ lib/class/quoteRequest.php
@@ -0,0 +1,207 @@
+<?php
+
+	class quoteRequest {
+
+		private string $setType;
+		private databaseManager $db;
+
+		public string $dbQuoteResquestId; // Used when updating the table incase the quoteRequestId has been changed after instantiation
+		
+		public bool $existed; // Can be used to see whether the given entity existed already at the time of instantiation
+
+		// Main database attributes
+		public $quoteRequestId;
+		public $businessId;
+		public $name;
+		public $email;
+		public $dateTimeAdded;
+
+		// Arrays to store linked data
+		public $services = array();
+
+		// -------------------------------------------------------------------------------------------------------------------------------------------------------
+		// -------------------------------------------------------------------------------------------------------------------------------------------------------
+		// Init variables
+		// -------------------------------------------------------------------------------------------------------------------------------------------------------
+		// -------------------------------------------------------------------------------------------------------------------------------------------------------
+
+		function __construct(string $quoteRequestId = '') {
+
+			// Connect to the database
+			require_once dirname(__FILE__)."/../manager/databaseManager.php";
+			$this->db = new databaseManager;
+
+			// Fetch from database
+			$fetch = $this->db->select('quoteRequest', '*', "WHERE quoteRequestId ='".$this->db->sanitize($quoteRequestId)."'");
+
+			// If quoteRequestId already exists then set the set method type to UPDATE and fetch the values for the quoteRequest
+			if ($fetch) {
+				$this->quoteRequestId = $quoteRequestId;
+				$this->businessId = $fetch[0]['businessId'];
+				$this->linkedToCustomerId = $fetch[0]['linkedToCustomerId'];
+				$this->name = $fetch[0]['name'];
+				$this->email = $fetch[0]['email'];
+				$this->address1 = $fetch[0]['address1'];
+				$this->address2 = $fetch[0]['address2'];
+				$this->state = $fetch[0]['state'];
+				$this->zipCode = $fetch[0]['zipCode'];
+				$this->dateTimeAdded = $fetch[0]['dateTimeAdded'];
+
+				$this->setType = 'UPDATE';
+				$this->existed = true;
+
+			// If quoteRequestId does not exist then set the set method type to INSERT and inititialize default values
+			} else {
+				// Make a new quoteRequestId
+				require_once dirname(__FILE__)."/tableUuid.php";
+				$uuid = new tableUuid('quoteRequest', 'quoteRequestId');
+				$this->quoteRequestId = $uuid->generatedId;
+				// Default businessId to the currently selected business
+				if (isset($_SESSION['ultiscape_businessId'])) {
+					$this->businessId = $_SESSION['ultiscape_businessId'];
+				} else {
+					$this->businessId = '';
+				}
+				// Default linkedToCustomerId to the currently selected business
+				if (isset($_SESSION['ultiscape_customerId'])) {
+					$this->linkedToCustomerId = $_SESSION['ultiscape_customerId'];
+				} else {
+					$this->linkedToCustomerId = '';
+				}
+				$this->name = NULL;
+				$this->email = NULL;
+				$this->address1 = NULL;
+				$this->address2 = NULL;
+				$this->state = NULL;
+				$this->zipCode = NULL;
+				// Default dateTimeAdded to now since it is likely going to be inserted at this time
+				$currentDateTime = new DateTime();
+				$this->dateTimeAdded = $currentDateTime->format('Y-m-d H:i:s');
+
+				$this->setType = 'INSERT';
+				$this->existed = false;
+			}
+
+			$this->$dbQuoteResquestId = $this->quoteRequestId;
+			
+		}
+
+		// -------------------------------------------------------------------------------------------------------------------------------------------------------
+		// -------------------------------------------------------------------------------------------------------------------------------------------------------
+		// Linked data pull functions
+		// -------------------------------------------------------------------------------------------------------------------------------------------------------
+		// -------------------------------------------------------------------------------------------------------------------------------------------------------
+
+		// services
+		public function pullServices ($params = '') {
+			$this->services = array();
+			// Add space before params
+			if ($params != '') {
+				$params = " ".$params;
+			}
+			// If there are entries, push them to the array
+			$fetch = $this->db->select('quoteRequestService', 'quoteRequestServiceId', "WHERE quoteResquestId = '$this->dbQuoteResquestId'".$params);
+			if ($fetch) {
+				foreach ($fetch as $row) {
+					array_push($this->services, $row['quoteRequestServiceId']);
+				}
+				return true;
+			} elseif ($this->db->getLastError() === '') {
+					return true;
+			} else {
+				return $this->db->getLastError();
+			}
+		}
+		
+		// -------------------------------------------------------------------------------------------------------------------------------------------------------
+		// -------------------------------------------------------------------------------------------------------------------------------------------------------
+		// Set function
+		// -------------------------------------------------------------------------------------------------------------------------------------------------------
+		// -------------------------------------------------------------------------------------------------------------------------------------------------------
+
+		public function set() {
+
+			$attributes = array(
+				'quoteRequestId' => $this->db->sanitize($this->dbQuoteResquestId),
+				'businessId' => $this->db->sanitize($this->businessId),
+				'name' => $this->db->sanitize($this->name),
+				'email' => $this->db->sanitize($this->email),
+				'dateTimeAdded' => $this->db->sanitize($this->dateTimeAdded)
+			);
+
+			if ($this->setType == 'UPDATE') {
+
+				// Update the values in the database after sanitizing them
+				if ($this->db->update('quoteRequest', $attributes, "WHERE quoteRequestId = '".$this->db->sanitize($this->dbQuoteResquestId)."'", 1)) {
+					return true;
+				} elseif ($this->db->getLastError() === '') {
+					return true;
+				} else {
+					return $this->db->getLastError();
+				}
+
+			} else {
+
+				// Insert the values to the database after sanitizing them
+				if ($this->db->insert('quoteRequest', $attributes)) {
+					// Set the setType to UPDATE since it is now in the database
+					$this->setType = 'UPDATE';
+					return true;
+				} else {
+					return $this->db->getLastError();
+				}
+
+			}
+
+		}
+
+		// -------------------------------------------------------------------------------------------------------------------------------------------------------
+		// -------------------------------------------------------------------------------------------------------------------------------------------------------
+		// Delete function
+		// -------------------------------------------------------------------------------------------------------------------------------------------------------
+		// -------------------------------------------------------------------------------------------------------------------------------------------------------
+
+		public function delete() {
+
+			// Remove row from database
+			if (!$this->db->delete('quoteRequest', "WHERE quoteRequestId = '".$this->db->sanitize($this->dbQuoteResquestId)."'", 1)) {
+				return $this->db->getLastError();
+			}
+
+			// Generate a new random id
+			require_once dirname(__FILE__)."/tableUuid.php";
+			$uuid = new tableUuid('quoteRequest', 'quoteRequestId');
+			$this->quoteRequestId = $uuid->generatedId;
+
+			// Reset all variables
+			// Default businessId to the currently selected business
+			if (isset($_SESSION['ultiscape_businessId'])) {
+				$this->businessId = $_SESSION['ultiscape_businessId'];
+			} else {
+				$this->businessId = '';
+			}
+			$this->linkedToCustomerId = NULL;
+			$this->name = NULL;
+			$this->email = NULL;
+			$this->address1 = NULL;
+			$this->address2 = NULL;
+			$this->state = NULL;
+			$this->zipCode = NULL;
+			// Default dateTimeAdded to now since it is likely going to be inserted at this time
+			$currentDateTime = new DateTime();
+			$this->dateTimeAdded = $currentDateTime->format('Y-m-d H:i:s');
+
+			// Clear arrays
+			$this->services = array();
+
+			// Set setType to INSERT since there is no longer a row to update
+			$this->setType = 'INSERT';
+
+			// Set existed to false since it no longer exists
+			$this->existed = false;
+
+			return true;
+		}
+	}
+
+?>
