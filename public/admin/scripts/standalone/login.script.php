978e4956a508900b0cd80ba8b0fb982c3d89d3b1
CameronMcGehee <cameronmcgehee23@gmail.com>
2021-11-08 15:45:25 -0500

    log adminLoginAttempts in admin login script

--- public/admin/scripts/standalone/login.script.php
+++ public/admin/scripts/standalone/login.script.php
@@ -5,37 +5,65 @@
 
     $adminLoginManager = new adminLoginManager();
 
+    $result = 'incomplete';
+
     // Check if input is set and valid
     if ( (!isset($_POST['usernameEmail']) || strlen($_POST['usernameEmail']) <= 5) || (!isset($_POST['password']) || strlen($_POST['password']) <= 5) ) {
-        header("location: ../../login?e=input");
-        exit();
+        $result = 'inputInvalid';
     }
 
-    $usernameCheck = $adminLoginManager->usernameExists($_POST['usernameEmail'], true); // These return the id so we know what admin they belong to
-    $emailCheck = $adminLoginManager->emailExists($_POST['usernameEmail'], true);
+    if ($result == 'incomplete') {
+        $usernameCheck = $adminLoginManager->usernameExists($_POST['usernameEmail'], true); // These return the id so we know what admin they belong to
+        $emailCheck = $adminLoginManager->emailExists($_POST['usernameEmail'], true);
+
+        // Check if email or username exists
+        if (!$usernameCheck && !$emailCheck) {
+            $result = 'noEmailUsername';
+        }
+    }
+
+    if ($result == 'incomplete') {
+        if ($usernameCheck) {
+            $adminIdToCheck = $usernameCheck;
+        } else {
+            $adminIdToCheck = $emailCheck;
+        }
+
+        //Check if the username/email and password match
+
+        if (password_verify($_POST['password'], $adminLoginManager->getPassword($adminIdToCheck))) {
+            // Login the user
+            $adminLoginManager->login($adminIdToCheck);
+            $result = 'success';
+
+            // if remember me is checked, save the login and set a cookie
+            if (false) { // Will do later
+                // Set a saved login
+            }
+        } else {
+            $result = 'password';
+        }
+    }
     
-    // Check if email or username exists
-    if (!$usernameCheck && !$emailCheck) {
-        header("location: ../../login?e=noEmailUsername");
+    // Log the attempt
+    require_once '../../../../lib/table/adminLoginAttempt.php';
+    require_once '../../../../lib/etc/getClientIpAddress.php';
+    
+    $attempt = new adminLoginAttempt();
+    if (isset($adminIdToCheck)) {
+        $attempt->adminId = $adminIdToCheck;
+    }
+    $attempt->clientIp = getClientIpAddress();
+    $attempt->result = $result;
+    var_dump($attempt->set());
+
+    // Redirect
+    if ($result == 'success') {
+        header("location: ../../");
+        exit();
+    } else {
+        header("location: ../../login?e=".urlencode($result));
         exit();
     }
-
-    if ($usernameCheck) {
-        $adminIdToCheck = $usernameCheck;
-    } else {
-        $adminIdToCheck = $emailCheck;
-    }
-
-    //Check if the username/email and password match
-
-    if (password_verify($_POST['password'], $adminLoginManager->getPassword($adminIdToCheck))) {
-        $adminLoginManager->login($adminIdToCheck);
-    } else {
-        header("location: ../../login?e=password");
-        exit();
-    }
-
-    header("location: ../../");
-    exit();
 
 ?>
