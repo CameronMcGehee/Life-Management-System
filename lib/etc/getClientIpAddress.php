7eed25bc27ae576ed663b8127544612232c6d2cd
CameronMcGehee <cameronmcgehee23@gmail.com>
2021-11-08 15:44:08 -0500

    add etc folder for misc functions and getIp function

---
+++ lib/etc/getClientIpAddress.php
@@ -0,0 +1,16 @@
+<?php
+
+    function getClientIpAddress(){
+        if(!empty($_SERVER['HTTP_CLIENT_IP'])){
+            //ip from share internet
+            $ip = $_SERVER['HTTP_CLIENT_IP'];
+        }elseif(!empty($_SERVER['HTTP_X_FORWARDED_FOR'])){
+            //ip pass from proxy
+            $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
+        }else{
+            $ip = $_SERVER['REMOTE_ADDR'];
+        }
+        return $ip;
+    }
+
+?>
