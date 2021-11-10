4e205bab17d21e25600b4396545d716499e831b6
CameronMcGehee <cameronmcgehee23@gmail.com>
2021-11-10 10:38:15 -0500

    fix url redirect to select business page

--- lib/manager/adminLoginManager.php
+++ lib/manager/adminLoginManager.php
@@ -21,7 +21,7 @@
 			if (!isset($_SESSION['ultiscape_adminId']) && !isset($_SESSION['ultiscape_businessId'])) {
 				header("location: ".$loginLocation."login");
 			} elseif (isset($_SESSION['ultiscape_adminId']) && !isset($_SESSION['ultiscape_businessId'])) {
-				header("location: ".$businessSelectLocation."selectBusiness");
+				header("location: ".$businessSelectLocation."selectbusiness");
 			}
 		}
 
