f1e250294f1750600eb1e2c1bab631337d6d3746
Cameron McGehee <cameronmcgehee23@gmail.com>
2021-11-14 11:12:49 -0500

    fix create business script

--- public/admin/createbusiness/scripts/standalone/createbusiness.script.php
+++ public/admin/createbusiness/scripts/standalone/createbusiness.script.php
@@ -5,7 +5,7 @@
     $result = 'incomplete'; // Incomplete means there has not yet been an error so the script will continue.
 
     // Check if input is set and valid
-    if ( (!isset($_POST['businessName']) || strlen($_POST['businessName']) < 6) || (!isset($_POST['authToken']) || strlen($_POST['authToken']) < 17)) {
+    if ( (!isset($_POST['businessName']) || strlen($_POST['businessName']) < $ULTISCAPECONFIG['businessNameMinLength']) || (!isset($_POST['authToken']) || strlen($_POST['authToken']) < 17)) {
         $result = 'inputInvalid';
     }
 
@@ -22,21 +22,33 @@
         require_once '../../../../../lib/table/business.php';
         $newBusiness = new business();
         $newBusiness->displayName = $_POST['businessName'];
-        if ($newBusiness->set()) {
+        $newBusiness->adminDisplayName = $_POST['businessName'];
+        
+        if ($newBusiness->set() !== true) {
+            $result = 'setError';
+        }
+    }
+
+    if ($result == 'incomplete') {
+        // Link the current admin to the new business
+        require_once '../../../../../lib/database.php';
+        $db = new database();
+        if ($db->insert("adminBusinessBridge", array("businessId" => $newBusiness->businessId, "adminId" => $_SESSION['ultiscape_adminId'], "adminIsOwner" => 1))) {
             $result = 'success';
             // Set the current business to the new one
             $_SESSION['ultiscape_businessId'] = $newBusiness->businessId;
         } else {
-            $result = 'unknown';
+            $result = 'linkError';
+            $newBusiness->delete();
         }
     }
     
     // Redirect
     if ($result == 'success') {
-        header("location: ../../../?popup=businessCreated");
+        header("location: ../../../overview?popup=businessCreated");
         exit();
     } else {
-        header("location: ../../?e=".urlencode($result));
+        header("location: ../../../createBusiness?e=".urlencode($result));
         exit();
     }
 
