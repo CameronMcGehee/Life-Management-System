dff587e49d4f6333ab204a64c22a248024d9e4fb
Cameron McGehee <cameronmcgehee23@gmail.com>
2021-11-14 23:31:23 -0500

    updates to syling of column pages and edit business page

--- public/admin/editbusiness/index.php
+++ public/admin/editbusiness/index.php
@@ -25,7 +25,7 @@
 </head>
 
 <body>
-	<div class="appNoSidebarBodyWrapper">
+	<div class="cmsBodyWrapper">
 
 		<?php 
 			$currentBusiness = new business($_SESSION['ultiscape_businessId']);
@@ -33,108 +33,153 @@
 			echo $adminUIRender->renderAdminTopBar('../../', true, true, true);
 		?>
 
+		<?php 
+            echo $adminUIRender->renderAdminSideBar('../../');
+        ?>
+
 		<div class="cmsMainContentWrapper textColorThemeGray styledText">
-			
-			<div class="twoColPage-Info-Content maxHeight">
-				<div id="twoColInfoWrapper" class="paddingLeftRight90 paddingTopBottom90">
-				
-					<h1>Edit <i><?php echo htmlspecialchars($currentBusiness->adminDisplayName); ?></i></h1>
+			<form class="defaultForm maxHeight" action="./" method="POST">
+				<div class="twoColPage-Info-Content maxHeight">
+					<div id="twoColInfoWrapper" class="paddingLeftRight90 paddingTopBottom90">
+					
+						<h1>Edit <i><?php echo htmlspecialchars($currentBusiness->adminDisplayName); ?></i></h1>
 
-				</div>
+						<!-- <br> -->
 
-				<div id="twoColContentWrapper" class="padding90">
-					<form class="defaultForm" action="./" method="POST">
+						<button class="mediumButtonWrapper greenButton centered defaultMainShadows" type="submit">Save Changes</button>
 
-						<label for="displayName"><p>Business Name <span style="color: rgb(167, 0, 0);">*</span></p></label>
-						<input class="bigInput" type="text" name="displayName" id="displayName" placeholder="Business name..." style="width: 70%;" required value="<?php echo htmlspecialchars($currentBusiness->displayName); ?>">
-						<br><br>
+					</div>
 
-						<label for="adminDisplayName"><p>Internal Display Name (What you see in Ultiscape)</p></label>
-						<input class="defaultInput" type="text" name="adminDisplayName" id="adminDisplayName" placeholder="Internal display name..." value="<?php echo htmlspecialchars($currentBusiness->adminDisplayName); ?>">
-						<br><br><br>
+					<div id="twoColContentWrapper" class="paddingLeftRight90 maxHeight" style="overflow: auto;">
+						<div class="paddingTopBottom90">
+							<h3>General Info</h3>
+							<br>
+							<div style="border: 1px solid green; padding: 1em; width: 93%;">
 
-						<div style="border: 1px solid gray; width: 25em; height: 5em; padding: 1em;">
+								<label for="displayName"><p>Business Name <span style="color: rgb(167, 0, 0);">*</span></p></label>
+								<input class="bigInput" type="text" name="displayName" id="displayName" placeholder="Business name..." style="width: 70%;" required value="<?php echo htmlspecialchars($currentBusiness->displayName); ?>">
+								<br><br>
 
-							<img src="<?php if ($currentBusiness->fullLogoFile === NULL) {echo "../../images/ultiscape/etc/noLogo.png";} else echo "../../images/ultiscape/uploads/businessFullLogoFile/".htmlspecialchars($currentBusiness->fullLogoFile); ?>" style="height: 100%; float: left;">
+								<label for="adminDisplayName"><p>Internal Display Name (What you see in Ultiscape)</p></label>
+								<input class="defaultInput" type="text" name="adminDisplayName" id="adminDisplayName" placeholder="Internal display name..." value="<?php echo htmlspecialchars($currentBusiness->adminDisplayName); ?>">
+								<br><br><br>
+
+								<div style="border: 1px solid gray; padding: 1em; width: 90%; max-width: 25em; height: 5em;">
+									<img src="<?php if ($currentBusiness->fullLogoFile === NULL) {echo "../../images/ultiscape/etc/noLogo.png";} else echo "../../images/ultiscape/uploads/businessFullLogoFile/".htmlspecialchars($currentBusiness->fullLogoFile); ?>" style="height: 100%; float: left;">
+									
+									<input type="checkbox" name="useNewLogo" id="useNewLogo"><label for="useNewLogo"> <p style="display: inline; clear: both;">Upload a new logo</p></label>
+									<br><br>
+
+									<label for="fullLogoFile" style="clear: both;"><p>Logo File</p></label>
+									<input type="file" name="fullLogoFile" id="fullLogoFile" style="clear: both;">
+								</div>
+
+								<br><br>
+
+								<div class="twoCol">
+
+									<div>
+										<label for="address1"><p>Address Line 1</p></label>
+										<input class="defaultInput" type="text" name="address1" id="address1" placeholder="Address Line 1..." style="width: 90%" value="<?php echo htmlspecialchars($currentBusiness->address1); ?>">
+										<br><br>
+
+										<label for="address2"><p>Address Line 2</p></label>
+										<input class="defaultInput" type="text" name="address2" id="address2" placeholder="" style="width: 90%" value="<?php echo htmlspecialchars($currentBusiness->address2); ?>">
+										<br><br>
+
+										<label for="zipCode"><p>Zip Code</p></label>
+										<input class="defaultInput" type="text" name="zipCode" id="zipCode" placeholder="Zip code..." style="width: 90%" value="<?php echo htmlspecialchars($currentBusiness->zipCode); ?>">
+									</div>
+
+									<div>
+										<label for="city"><p>City</p></label>
+										<input class="defaultInput" type="text" name="city" id="city" placeholder="City..." style="width: 90%" value="<?php echo htmlspecialchars($currentBusiness->city); ?>">
+										<br><br>
+
+										<label for="state"><p>State</p></label>
+										<input class="defaultInput" type="text" name="state" id="state" placeholder="State..." style="width: 90%" value="<?php echo htmlspecialchars($currentBusiness->state); ?>">
+									</div>
+									
+								</div>
+
+								<br>
+
+								<label for="phone1"><p>Phone Number</p></label>
+								<div class="fourColCompact">
+									<div>
+										<input class="defaultInput" type="text" name="phonePrefix" id="phonePrefix" placeholder="+1" value="1" style="width: 1.5em;">
+									</div>
+
+									<div>
+										<input class="defaultInput" type="text" name="phone1" id="phone1" placeholder="555" style="width: 3em;">
+									</div>
+
+									<div>
+										<input class="defaultInput" type="text" name="phone2" id="phone2" placeholder="555" style="width: 3em;">
+									</div>
+
+									<div>
+										<input class="defaultInput" type="text" name="phone3" id="phone3" placeholder="5555" style="width: 3em;">
+									</div>
+								</div>
+							</div>
 							
-							<input type="checkbox" name="useNewLogo" id="useNewLogo"><label for="useNewLogo"> <p style="display: inline; clear: both;">Upload a new logo</p></label>
 							<br><br>
 
-							<label for="fullLogoFile" style="clear: both;"><p>Logo File</p></label>
-							<input type="file" name="fullLogoFile" id="fullLogoFile" style="clear: both;">
-						</div>
-
-						<br><br>
-
-						<div class="twoCol">
-
-							<div>
-								<label for="address"><p>Address</p></label>
-								<input class="defaultInput" type="text" name="address" id="address" placeholder="Address..." style="width: 90%">
-								<br><br>
-
-								<label for="city"><p>City</p></label>
-								<input class="defaultInput" type="text" name="city" id="city" placeholder="City..." style="width: 90%">
-							</div>
-
-							<div>
-								<label for="state"><p>State</p></label>
-								<input class="defaultInput" type="text" name="state" id="state" placeholder="State..." style="width: 90%">
-								<br><br>
-
-								<label for="zipCode"><p>Zip Code</p></label>
-								<input class="defaultInput" type="text" name="zipCode" id="zipCode" placeholder="Zip code..." style="width: 90%">
+							<h3>Units</h3>
+							<br>
+							<div style="border: 1px solid green; padding: 1em; width: 93%;">
+								<p>Inputs</p>
 							</div>
 							
+							<br><br>
+
+							<h3>Customers</h3>
+							<br>
+							<div style="border: 1px solid green; padding: 1em; width: 93%;">
+								<p>Inputs</p>
+							</div>
+							
+							<br><br>
+
+							<h3>Staff</h3>
+							<br>
+							<div style="border: 1px solid green; padding: 1em; width: 93%;">
+								<p>Inputs</p>
+							</div>
+							
+							<br><br>
+
+							<h3>Crews</h3>
+							<br>
+							<div style="border: 1px solid green; padding: 1em; width: 93%;">
+								<p>Inputs</p>
+							</div>
+							
+							<br><br>
+							
+							<?php
+								// Generate an auth token for the form
+								require_once '../../../lib/table/authToken.php';
+								$token = new authToken();
+								$token->authName = 'editBusiness';
+								$token->set();
+							?>
+
+							<input type="hidden" name="authToken" id="authToken" value="<?php echo htmlspecialchars($token->authTokenId); ?>">
 						</div>
-
-						<br>
-
-						<label for="phone1"><p>Phone Number</p></label>
-						<div class="fourColCompact">
-							<div>
-								<input class="defaultInput" type="text" name="phonePrefix" id="phonePrefix" placeholder="+1" value="1" style="width: 3em;">
-							</div>
-
-							<div>
-								<input class="defaultInput" type="text" name="phone1" id="phone1" placeholder="555" style="width: 6em;">
-							</div>
-
-							<div>
-								<input class="defaultInput" type="text" name="phone2" id="phone2" placeholder="555" style="width: 6em;">
-							</div>
-
-							<div>
-								<input class="defaultInput" type="text" name="phone3" id="phone3" placeholder="5555" style="width: 6em;">
-							</div>
-						</div>
-						
-						<?php
-                            // Generate an auth token for the form
-                            require_once '../../../lib/table/authToken.php';
-							$token = new authToken();
-							$token->authName = 'createBusiness';
-							$token->set();
-                        ?>
-
-                        <input type="hidden" name="authToken" id="authToken" value="<?php echo htmlspecialchars($token->authTokenId); ?>">
-
-						<br><br>
-
-						<button class="mediumButtonWrapper greenButton centered defaultMainShadows" type="submit">Create!</button>
-					</form>
-
+					</div>
 				</div>
-			</div>
+			</form>
 			
 		</div>
 		
 		<?php
-			echo $adminUIRender->renderAdminFooter('../../', true, true);
+			echo $adminUIRender->renderAdminFooter('../../');
 		?>
 
 		<?php 
-			// echo $adminUIRender->renderMobileNavBar('../../');
+			echo $adminUIRender->renderAdminMobileNavBar('../../');
 		?>
 
 	</div>
