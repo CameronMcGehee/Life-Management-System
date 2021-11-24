18c74f96b41c2c2ed81dfb940363d834eabc0ad0
Cameron McGehee <cameronmcgehee23@gmail.com>
2021-11-24 00:24:23 -0500

    working register page and script

---
+++ public/admin/createaccount/index.php
@@ -0,0 +1,174 @@
+<?php
+
+	// Start Session
+	require_once '../../php/startSession.php';
+
+	// This is the login page so if we are already signed in, just redirect to the business selection page
+
+	if (isset($_SESSION['ultiscape_adminId'])) {
+		header("location: ../");
+	}
+
+	require_once '../../../lib/adminUIRender.php';
+	$adminUIRender = new adminUIRender();
+
+	echo $adminUIRender->renderAdminHtmlTop('../../', 'Login', 'Login to your UltiScape account.');
+
+?>
+
+	<link rel="stylesheet" type="text/css" href="../../css/app/admin/adminRegisterPage.css">
+
+	<style>
+		#loadingGif {
+			margin-left: none;
+			margin-right: auto;
+		}
+		
+		/* Only for desktop, make the loading gif go to the right */
+		@media only screen and (min-width: 1000px) {
+			#loadingGif {
+				margin-left: auto;
+				margin-right: 0px;
+			}
+		}
+	</style>
+
+	<script src="../../js/etc/animation/shake.js"></script>
+
+	<script src="../../js/etc/form/showFormError.js"></script>
+	<script src="../../js/etc/form/clearFormErrors.js"></script>
+
+	<script>
+
+		var formData;
+		var formOutput;
+		var url = new URL(window.location.href);
+
+		$(document).ready(function() {
+			$("#registerForm").submit(function(event) {
+				event.preventDefault();
+				$('.loadingGif').fadeIn(100);
+				formData = $("#registerForm").serialize();
+				
+				$("#scriptLoader").load("./scripts/async/register.script.php", {
+					formData: formData
+				}, function () {
+					formOutput = $("#scriptLoader").html();
+					clearFormErrors();
+
+					switch (formOutput) {
+						case 'success':
+							window.location.replace('../');
+							break;
+						case 'emailValidity':
+							showFormError("#emailError", "#email");
+							$("#email").shake(50);
+							break;
+						case 'emailExists':
+							showFormError("#emailExistsError", "#email");
+							$("#email").shake(50);
+							break;
+						case 'usernameExists':
+							showFormError("#usernameExistsError", "#username");
+							$("#username").shake(50);
+							break;
+						default:
+							showFormError("#"+formOutput+"Error", "#"+formOutput);
+							$("#"+formOutput).shake(50);
+							break;
+					}
+
+					$('.loadingGif').fadeOut(100);
+				});
+			});
+		});
+	</script>
+</head>
+
+<body>
+	<span style="display: none;" id="scriptLoader"></span>
+	<div class="appNoSidebarBodyWrapper">
+
+		<?php 
+			echo $adminUIRender->renderAdminTopBar('../../', true, false, false);
+		?>
+
+		<div class="cmsMainContentWrapper">
+			
+		<div class="maxHeight xyCenteredFlex flexDirectionColumn marginLeftRight90 styledText textColorThemeGray">
+				<div class="cmsRegisterFormArea defaultMainShadows" style="margin-left: 2em; margin-right: 2em;">
+					
+					<h1 class="centered">Create an Account</h1>
+
+					<br><br><br>
+					
+					<form class="defaultForm" method="POST" action="./" id="registerForm">
+
+						<div class="twoCol">
+							<div style="padding-right: .5em;">
+								<label for="firstName"><p>First Name</p></label>
+								<input class="defaultInput" type="text" name="firstName" id="firstName" placeholder="First Name..." style="width: 100%;">
+								<span id="firstNameError" class="underInputError" style="display: none;">Your first name is too short.</span>
+							</div>
+
+							<div style="padding-left: .5em;">
+								<label for="lastName"><p>Last Name</p></label>
+								<input class="defaultInput" type="text" name="lastName" id="lastName" placeholder="Last Name..." style="width: 100%;">
+								<span id="lastNameError" class="underInputError" style="display: none;">Your last name is too short.</span>
+							</div>
+						</div>
+
+						<br>
+						
+						<label for="email"><p>Email</p></label>
+						<input class="defaultInput" type="text" name="email" id="email" placeholder="Email..." style="width: 100%;">
+						<span id="emailError" class="underInputError" style="display: none;">Input a valid email address.</span>
+						<span id="emailExistsError" class="underInputError" style="display: none;">This email is already in use.</span>
+
+						<br><br>
+					
+						<label for="username"><p>Username</p></label>
+						<input class="defaultInput" type="text" name="username" id="username" placeholder="Username..." style="width: 100%;">
+						<span id="usernameError" class="underInputError" style="display: none;">Your username is too short.</span>
+						<span id="usernameExistsError" class="underInputError" style="display: none;">This username is already in use.</span>
+						
+						<br><br>
+						
+						<label for="password"><p>Password</p></label>
+						<input class="defaultInput" type="password" name="password" id="password" placeholder="Password..." style="width: 100%;">
+						<span id="passwordError" class="underInputError" style="display: none;">Your password is too short.</span>
+
+						<?php
+							// Generate an auth token for the form
+							require_once '../../../lib/table/authToken.php';
+							$token = new authToken();
+							$token->authName = 'adminRegister';
+							$token->set();
+						?>
+
+						<input type="hidden" name="authToken" id="authToken" value="<?php echo htmlspecialchars($token->authTokenId); ?>">
+						
+						<br><br>
+						
+						<button class="smallButtonWrapper greenButton xyCenteredFlex centered defaultMainShadows" type="submit">Start using UltiScape!</button>
+					
+					</form>
+
+				</div>
+			</div>
+		
+		</div>
+		
+		<?php
+			echo $adminUIRender->renderAdminFooter('../../', true, true);
+		?>
+
+		<?php 
+			// echo $adminUIRender->renderMobileNavBar('../../');
+		?>
+
+	</div>
+</body>
+<?php 
+	echo $adminUIRender->renderAdminHtmlBottom('../../');
+?>
---
+++ public/admin/createaccount/scripts/async/register.script.php
@@ -0,0 +1,98 @@
+<?php
+
+	require_once '../../../../php/startSession.php';
+
+	require_once '../../../../../lib/table/admin.php';
+	$admin = new admin();
+
+	if (!isset($_POST['formData'])) {
+		echo 'noData';
+		exit();
+	}
+	parse_str($_POST['formData'], $formData);
+
+	// Validate the auth token
+	require_once '../../../../../lib/etc/authToken/validateAuthToken.php';
+	if (!validateAuthToken($formData['authToken'], 'adminRegister')) {
+		echo 'tokenInvalid';
+		exit();
+	}
+
+	// Check if input is set and valid
+
+	if (!isset($formData['firstName']) || strlen($formData['firstName']) < (int)$ULTISCAPECONFIG['firstNameMinLength']) {
+		echo 'firstName';
+		exit();
+	}
+	$admin->firstName = $formData['firstName'];
+
+	if (!isset($formData['lastName']) || strlen($formData['lastName']) < (int)$ULTISCAPECONFIG['lastNameMinLength']) {
+		echo 'lastName';
+		exit();
+	}
+	$admin->lastName = $formData['lastName'];
+	
+	if (!isset($formData['email']) || strlen($formData['email']) < (int)$ULTISCAPECONFIG['emailMinLength']) {
+		echo 'email';
+		exit();
+	}
+
+	// If enabled, use filter function to make sure email is an actual email
+	if ($ULTISCAPECONFIG['useEmailValidation']) {
+		if (!filter_var($formData['email'], FILTER_VALIDATE_EMAIL)) {
+			echo 'emailValidity';
+			exit();
+		}
+	}
+	$admin->email = $formData['email'];
+
+	if (!isset($formData['username']) || strlen($formData['username']) < (int)$ULTISCAPECONFIG['usernameMinLength']) {
+		echo 'username';
+		exit();
+	}
+	$admin->username = $formData['username'];
+
+	if (!isset($formData['password']) || strlen($formData['password']) < (int)$ULTISCAPECONFIG['passwordMinLength']) {
+		echo 'password';
+		exit();
+	}
+	$admin->password = password_hash($formData['password'], PASSWORD_DEFAULT);
+
+	// if (!isset($formData['nameTitle'])) {
+	// 	$nameTitle = '';
+	// } elseif ($formData['nameTitle'] == '') {
+	// 	if (strlen($formData['nameTitle']) < (int)$ULTISCAPECONFIG['nameTitleMinLength']) {
+	// 		echo 'nameTitle';
+	// 		exit();
+	// 	}
+	// }
+
+	require_once '../../../../../lib/database.php'; $db = new database();
+
+	// Check for the email in the database
+
+	if ($db->select('admin', 'email', "WHERE email = '".$db->sanitize($formData['email'])."'")) {
+		echo 'emailExists';
+		exit();
+	}
+
+	// Check for the username in the database
+
+	if ($db->select('admin', 'username', "WHERE username = '".$db->sanitize($formData['username'])."'")) {
+		echo 'usernameExists';
+		exit();
+	}
+
+	// Use the auth token
+	require_once '../../../../../lib/etc/authToken/useAuthToken.php';
+	useAuthToken($formData['authToken'], 'adminRegister');
+
+	if (!$admin->set()) {
+		echo 'setError';
+		exit();
+	}
+
+	echo 'success';
+	exit();
+
+?>
