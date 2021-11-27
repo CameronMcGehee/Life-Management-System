671f4de0e0d9938fb058c497776b0bcc52cfd151
Cameron McGehee <cameronmcgehee23@gmail.com>
2021-11-27 01:15:34 -0500

    sql to update the database after changes for v0.1.6

---
+++ lib/config/update/db/0.1.6.sql
@@ -0,0 +1,8 @@
+ALTER TABLE `business` DROP `modEstExtName`;
+ALTER TABLE `business` DROP `modPropExtName`;
+ALTER TABLE `business` DROP `modStaffExtName`;
+ALTER TABLE `business` DROP `modCrewsExtName`;
+
+ALTER TABLE `admin` DROP `surname`;
+ALTER TABLE `customer` DROP `surname`;
+ALTER TABLE `staff` DROP `surname`;
