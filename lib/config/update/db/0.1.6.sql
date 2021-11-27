2ca8f1f05b4fb570901e77be1957dd494b120cdc
Cameron McGehee <cameronmcgehee23@gmail.com>
2021-11-27 14:58:35 -0500

    changing varchars to text for UX and encryption

--- lib/config/update/db/0.1.6.sql
+++ lib/config/update/db/0.1.6.sql
@@ -6,3 +6,5 @@ ALTER TABLE `business` DROP `modCrewsExtName`;
 ALTER TABLE `admin` DROP `surname`;
 ALTER TABLE `customer` DROP `surname`;
 ALTER TABLE `staff` DROP `surname`;
+
+-- Data encryption and extra space for UX - So many changes (changing varchars to text) you must drop and rebuild the entire database. I'm not writing all of this in alter statements.
