140c25c314bf869ceab0a025a11ee9b1802ce856
Cameron McGehee <cameronmcgehee23@gmail.com>
2022-01-31 17:20:12 -0500

    didYouKnowBox render class done

--- lib/render/etc/didYouKnowBox.php
+++ lib/render/etc/didYouKnowBox.php
@@ -4,20 +4,32 @@
 
 	class didYouKnowBox extends render {
 
-		public string $path;
-		public $allowedTags;
-		public string $style;
+		public string $renderId;
+		public array $options;
 
-		function __construct(string $path = './', $allowedTags = NULL, string $style = '') {
-			$this->path = $path;
-			$this->allowedTags = $allowedTags;
-			$this->style = $style;
+		private array $phrases = [
+			"With UltiScape, you can add tags to each customer to sort them into relevant categories.",
+			"UltiScape is still in early development, so please be patient if something doesn't work as intended."
+		];
+
+		function __construct(string $renderId, array $options) {
+			$this->renderId = $renderId;
+
+			if (empty($options['rootPathPrefix'])) {
+				$options['rootPathPrefix'] = './';
+			}
+
+			if (empty($options['style'])) {
+				$options['style'] = '';
+			}
+
+			$this->options = $options;
 		}
 
 		function render() {
 
-			$this->output = '';
-
+			$phrase = rand(0, count($this->phrases)-1);
+			$this->output = '<div class="grayInfoBox defaultMainShadows desktopOnlyBlock"><h3>Did you know?</h3><br><p>'.$this->phrases[$phrase].'</p></div>';
 			
 		}
 	}
