edb2796f1f2f5650af17d363e480df7470c6154c
Cameron McGehee <cameronmcgehee23@gmail.com>
2022-02-28 22:39:46 -0500

    Create validateDate.php

---
+++ lib/etc/time/validateDate.php
@@ -0,0 +1,8 @@
+<?php
+
+    function validateDate($date, $format = 'Y-m-d'){
+        $d = DateTime::createFromFormat($format, $date);
+        return $d && $d->format($format) === $date;
+    }
+
+?>
