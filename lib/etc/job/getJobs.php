3e5006f163eceac2eae2638a7b9543fb52f03a47
Cameron McGehee <cameronmcgehee23@gmail.com>
2022-03-01 18:11:59 -0500

    Create getJobs.php

---
+++ lib/etc/job/getJobs.php
@@ -0,0 +1,35 @@
+<?php
+
+    // Get a full list of jobs within a date span (start and end dates are included) (overdue jobs do not have to be within the date span!)
+
+		function getJobs($startDateTime, $endDateTime, array $exclude = array()) { // "singular", "recurring", "completed", "cancelled", "overdue"
+			
+            require_once dirname(__DIR__).'../../database.php';
+            $db = new database();
+            
+            $startDateTimeSan = $db->sanitize($startDateTime);
+			$endDateTimeSan = $db->sanitize($endDateTime);
+
+			$jobs = [];
+
+            // Get all overdue jobs
+            $fetch = $db->select('job', '*', "WHERE businessId = '".$_SESSION['ultiscape_businessId']."' AND (startDateTime < '".$startDateTimeSan."' AND completedDate = NULL)");
+            if ($fetch) {
+                $jobs = array_merge($jobs, $fetch);
+            }
+
+            // Get all jobs from the jobs table that match the date range
+            $fetch = $db->select('job', '*', "WHERE businessId = '".$_SESSION['ultiscape_businessId']."' AND ((startDateTime >= '".$startDateTimeSan."' AND endDateTime <= '".$startDateTimeSan."') OR (endDateTime >= '".$startDateTimeSan."' AND endDateTime <= '".$endDateTimeSan."') OR (endDateTime >= '".$startDateTimeSan."' AND endDateTime = NULL))");
+            if ($fetch) {
+                $jobs = array_merge($jobs, $fetch);
+            }
+
+			if ($jobs) {
+                return $jobs;
+            } else {
+                return [];
+            }
+            
+		}
+
+?>
