02203177ed6474527820477afc84970198de4c17
Cameron McGehee <cameronmcgehee23@gmail.com>
2022-04-11 20:40:33 -0400

    getPaymentTotal invoice function

---
+++ lib/etc/invoice/getPaymentTotal.php
@@ -0,0 +1,36 @@
+<?php
+
+    // Get a full list of jobs within a date span (start and end dates are included) (overdue jobs do not have to be within the date span!)
+
+		function getPaymentTotal($invoiceId) {
+			
+            require_once dirname(__DIR__).'../../table/invoice.php';
+            require_once dirname(__DIR__).'../../table/payment.php';
+
+            // Check if the invoice exists
+            $invoice = new invoice($invoiceId);
+            if (!$invoice->existed) {
+                return false;
+            }
+
+            // Get payments
+            $invoice->pullPayments();
+
+            if (count($invoice->payments) < 1) {
+                return 0;
+            }
+
+            // For each payment, add to the total
+            $runningTotal = 0;
+            foreach ($invoice->payments as $paymentId) {
+                $payment = new payment($paymentId);
+                if ($payment->existed) {
+                    $runningTotal = $runningTotal + ($payment->amount);
+                }
+            }
+
+            return $runningTotal;
+            
+		}
+
+?>
