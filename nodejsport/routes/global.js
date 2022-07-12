fa1a7d1d2e4a0e8f889efa67ae9523edf475982c
Cameron McGehee <cameronmcgehee23@gmail.com>
2022-07-12 19:41:54 -0400

    redirect /login to appselect page

--- nodejsport/routes/global.js
+++ nodejsport/routes/global.js
@@ -8,6 +8,14 @@ router.get('/', (req, res) => {
     });
 });
 
+// If someone goes to /login but doesn't specify a type, redirect to the appselect page
+router.get('/login', (req, res) => {
+    res.render('appselect', {
+        layout: 'global',
+        title: "Welcome to Ultiscape!"
+    });
+});
+
 // Catch all unknown routes and redirect to an error page
 router.get('*', function(req, res) {
 	res.render('unknownpage', {
