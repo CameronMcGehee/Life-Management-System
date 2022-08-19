5a9f54c9e23e2ff1d89f8c5162d2126b8294197e
Cameron McGehee <cameronmcgehee23@gmail.com>
2022-08-18 22:25:54 -0400

    move react frontend to separate repository

--- nodejsport/ultiscape/src/js/etc/form/clearFormErrors.js
+++
@@ -1,6 +0,0 @@
-function clearFormErrors() {
-    // For each element with the .underInputError class, hide it
-    $('.underInputError').each(function(i, obj) {
-        $(this).hide(0);
-    });
-}
